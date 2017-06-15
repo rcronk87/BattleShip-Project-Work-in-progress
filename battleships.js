4d55e6f0a6860143d8081b2953f2448655d1f6ae
Brianna Kutauskas <briannakutauskas@Briannas-MacBook-Pro.local>
2017-06-15 06:38:47 -0700

    working first epic

--- battleships.js
+++ battleships.js
@@ -1,43 +1,47 @@
 // This is the controller
 $(document).ready(function(){
   tableLoop();
+  boardData();
+  callShip();
 // This is the click to change color to red and counts torpedos
   $("td").click(function() {
+    // This splits the class into coordinates
     var split = $(this).attr('class').split("");
+    var row = split[0];
+    var column = split[1];
     console.log(split);
-    $(this).css("background-color", "red")
-    $(this).addClass("hit");
-    if(shipLocation === split){
-      alert("hit");
+    //checks for a hit and changes color to red
+    if (board[row][column] === ship) {
+      console.log('HIT!');
+      hits++;
+      $(this).css("background-color", "red");
+      // $(this).addClass("hit");
+      if (hits === 5){
+        console.log("You win!");
+      }
     }
-    if($("td").hasClass("hit") === true) {
+    //checks for a miss and changes color to blue
+    else{
+      console.log("MISS!");
+      $(this).css("background-color", "#00c0ff");
+      if(torpedos === 25){
+        console.log("You lose!!");
+        $(".shipLocation").css("border-color", "red");
+      }
+    }
       $("#torpedos").text("Torpedos used: " + torpedos++);
       $(this).off("click");
-    }
+  });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-});
 // This is the model
 // fucntion to makle the table 10 x 10
 var torpedos = 1;
-var index;
+var hits;
 var board = [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]];
 const ship = 1;
-var shipPosition = Math.floor(Math.random()*100)
 function tableLoop() {
-  var table = $("<table> </table>")
+  var table = $("<table> </table>");
   // loop to make rows
   for (i = 0; i < 10; i++) {
     var row = $("<tr></tr>").addClass([i]);
@@ -56,22 +60,26 @@ function boardData() {
   for(var i= 0; i < 10; i++) {
     for(var j=1; j < 10; j++){
       board[i][j] = 0;
-    };
-  };
+    }
+  }
 }
-var shipLocation
+
+/**
+ * Create 5 ships in the board array
+ **/
 function callShip() {
   for (var i=0; i<5; i++){
+    // Do-while loop for the random numbers while board position is equal to 1
     var column = Math.floor(Math.random() * 10);
-    // column = column.toString();
     var row = Math.floor(Math.random() * 10);
-    // row = row.toString();
-    board[row][column] = ship
+    // TODO handle possible duplicates
+    board[row][column] = ship;
+    var pop = "." + row + column;
+    $(pop).addClass('shipLocation');
+    console.log(pop);
   }
-  console.log(shipLocation)
+  // console.log(board);
 }
-boardData();
-callShip();
 
 // function newCallShip() {
 //   var randomShip = Math.floor(Math.random() * 100);
